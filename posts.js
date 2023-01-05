9db4db56b983a101dc6a13e043176401aecf7257
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2023-01-05 23:13:19 +0100

    first commit

---
+++ posts.js
@@ -0,0 +1,32 @@
+posts = 
+
+[
+  {
+    title : 'Post 2',
+    body : 'Body of post',
+    category : 'Event',
+    tags : ['news', 'events'],
+    date : Date(),
+  },
+  {
+    title : 'Post 3',
+    body : 'Body of post',
+    category : 'Tech',
+    tags : ['news', 'events'],
+    date : Date(), 
+  },
+  {
+    title : 'Post 4',
+    body : 'Body of post',
+    category : 'Beauty',
+    tags : ['news', 'events'],
+    date : Date(), 
+  },
+  {
+    title : 'Post 5',
+    body : 'Body of post',
+    category : 'Lifestyle',
+    tags : ['news', 'events'],
+    date : Date(), 
+  },
+]
